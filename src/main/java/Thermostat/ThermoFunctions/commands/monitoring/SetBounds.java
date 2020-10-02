a202225a780588f8d07592275e9f03c23b68ed31
OpenSrcerer <weedpot@fhf.edu.al>
2020-10-02 19:25:29 +0300

    Improved readability and logic of setbounds minimum/maximum slowmode management command.

--- src/main/java/Thermostat/ThermoFunctions/commands/monitoring/SetBounds.java
+++ src/main/java/Thermostat/ThermoFunctions/commands/monitoring/SetBounds.java
@@ -8,11 +8,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import thermostat.preparedStatements.ErrorEmbeds;
 import thermostat.mySQL.DataSource;
+import thermostat.preparedStatements.HelpEmbeds;
 import thermostat.thermoFunctions.Messages;
 import thermostat.thermoFunctions.commands.CommandEvent;
 import thermostat.thermoFunctions.entities.CommandType;
 import thermostat.thermostat;
 
+import java.sql.SQLException;
 import java.time.Instant;
 import java.util.*;
 
@@ -26,17 +28,23 @@ public class SetBounds implements CommandEvent {
     private final Guild eventGuild;
     private final TextChannel eventChannel;
     private final Member eventMember;
+    private final String eventPrefix;
     private ArrayList<String> args;
 
     private EnumSet<Permission> missingThermostatPerms, missingMemberPerms;
 
     private static final EmbedBuilder embed = new EmbedBuilder();
 
-    public SetBounds(Guild eg, TextChannel tc, Member em, ArrayList<String> ag)
+    private enum ActionType {
+        INVALID, MINIMUM, MAXIMUM
+    }
+
+    public SetBounds(Guild eg, TextChannel tc, Member em, String px, ArrayList<String> ag)
     {
         eventGuild = eg;
         eventChannel = tc;
         eventMember = em;
+        eventPrefix = px;
         args = ag;
 
         checkPermissions();
@@ -74,98 +82,117 @@ public class SetBounds implements CommandEvent {
     @SuppressWarnings("unchecked")
     @Override
     public void execute() {
-        if (args.size() < 3) {
+        if (args.size() < 2) {
             Messages.sendMessage(eventChannel, ErrorEmbeds.bothChannelAndSlow());
             return;
         }
 
-        // command initiation with prefix removal
-        args.remove(0);
-
-        StringBuilder nonValid,
-                noText,
-                complete = new StringBuilder(),
+        StringBuilder nonValid = new StringBuilder(),
+                noText = new StringBuilder(),
+                minComplete = new StringBuilder(),
+                maxComplete = new StringBuilder(),
                 badSlowmode = new StringBuilder();
+        // type represents the action being taken
+        // setMaximum or setMinimum
+        ActionType type = ActionType.INVALID;
+        // value given to us by user to assign as slowmode
+        int argumentSlow;
 
-        boolean removed;
-        {
-            List<?> results = parseChannelArgument(eventGuild, args);
-
-            nonValid = (StringBuilder) results.get(0);
-            noText = (StringBuilder) results.get(1);
-            removed = (boolean) results.get(2);
-            args = (ArrayList<String>) results.get(3);
+        // #1 - Check the [minimum/maximum] argument
+        if (args.get(0).contains("max")) {
+            type = ActionType.MAXIMUM;
+        } else if (args.get(0).contains("min")) {
+            type = ActionType.MINIMUM;
         }
 
-        // Parsing the slowmode argument
-        int argumentSlow;
+        // #2 - Check the [slowmode] argument
         try {
-            argumentSlow = parseSlowmode(args.get(args.size() - 1));
+            argumentSlow = parseSlowmode(args.get(1));
         } catch (NumberFormatException ex) {
             Messages.sendMessage(eventChannel, ErrorEmbeds.invalidSlowmode());
             return;
         }
 
-        if (args.size() >= 2) {
-            for (int index = 0; index < args.size() - 1; ++index) {
-                try {
-                    int minimumSlow;
-                    minimumSlow = DataSource.queryInt("SELECT MIN_SLOW FROM CHANNEL_SETTINGS WHERE CHANNEL_ID = ?", args.get(index));
+        // #3 - Remove the [min/max] and [slowmode] arguments
+        args.subList(0, 1).clear();
 
-                    if (argumentSlow <= minimumSlow && argumentSlow <= 21600) {
-                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ?, MIN_SLOW = ? WHERE CHANNEL_ID = ?",
-                                Arrays.asList(Integer.toString(argumentSlow), Integer.toString(argumentSlow), args.get(index)));
-                        complete.append("<#").append(args.get(index)).append("> ");
-                    } else if (argumentSlow > minimumSlow && argumentSlow <= 21600) {
-                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ? WHERE CHANNEL_ID = ?",
-                                Arrays.asList(Integer.toString(argumentSlow), args.get(index)));
-                        complete.append("<#").append(args.get(index)).append("> ");
-                    } else {
-                        badSlowmode.append("<#").append(args.get(index)).append("> ");
-                    }
+        // #4 - Parse the optional <channels/categories> argument
+        if (!args.isEmpty())
+        {
+            List<?> results = parseChannelArgument(eventGuild, args);
 
-                } catch (Exception ex) {
-                    nonValid.append("\"").append(args.get(index)).append("\" ");
-                }
-            }
-        } else if (!removed) {
-            try {
-                int minimumSlow;
-                minimumSlow = DataSource.queryInt("SELECT MIN_SLOW FROM CHANNEL_SETTINGS WHERE CHANNEL_ID = ?", eventChannel.getId());
-
-                if (argumentSlow <= minimumSlow && argumentSlow <= 21600) {
-                    DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ?, MIN_SLOW = ? WHERE CHANNEL_ID = ?",
-                            Arrays.asList(Integer.toString(argumentSlow), Integer.toString(argumentSlow), eventChannel.getId()));
-                    complete.append("<#").append(eventChannel.getId()).append("> ");
-                } else if (argumentSlow > minimumSlow && argumentSlow <= 21600) {
-                    DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ? WHERE CHANNEL_ID = ?",
-                            Arrays.asList(Integer.toString(argumentSlow), eventChannel.getId()));
-                    complete.append("<#").append(eventChannel.getId()).append("> ");
-                } else {
-                    badSlowmode.append("<#").append(eventChannel.getId()).append("> ");
-                }
-            } catch (Exception ex) {
-                Messages.sendMessage(eventChannel, ErrorEmbeds.errFatal());
-                return;
-            }
+            nonValid = (StringBuilder) results.get(0);
+            noText = (StringBuilder) results.get(1);
+            args = (ArrayList<String>) results.get(2);
         }
 
+        // #5 - If no channel arguments were provided, add
+        // event channel as the target channel.
+        else {
+            args.add(eventChannel.getId());
+        }
+        // args now remains as a list of target channel(s).
+
+        // #6 - Perform the appropriate actions
+        int minimumSlow, maximumSlow;
+        
+        if (type != ActionType.INVALID) {
+            for (String arg : args) {
+                try {
+
+                    {
+                        List<Integer> channelSlowmodes = DataSource.queryInts("SELECT MIN_SLOW, MAX_SLOW FROM CHANNEL_SETTINGS WHERE CHANNEL_ID = ?", arg);
+
+                        minimumSlow = channelSlowmodes.get(0);
+                        maximumSlow = channelSlowmodes.get(1);
+                    }
+
+                    if (argumentSlow > 21600) {
+                        badSlowmode.append("<#").append(arg).append("> ");
+                    } else if (argumentSlow < minimumSlow && type == ActionType.MAXIMUM) {
+                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ?, MIN_SLOW = ? WHERE CHANNEL_ID = ?",
+                                Arrays.asList(Integer.toString(argumentSlow), Integer.toString(argumentSlow), arg));
+                        maxComplete.append("<#").append(arg).append("> ");
+                    } else if (argumentSlow >= minimumSlow && type == ActionType.MAXIMUM) {
+                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MAX_SLOW = ? WHERE CHANNEL_ID = ?",
+                                Arrays.asList(Integer.toString(argumentSlow), arg));
+                        maxComplete.append("<#").append(arg).append("> ");
+                    } else if (argumentSlow > maximumSlow) {
+                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MIN_SLOW = ?, MAX_SLOW = ? WHERE CHANNEL_ID = ?",
+                                Arrays.asList(Integer.toString(argumentSlow), Integer.toString(argumentSlow), arg));
+                        minComplete.append("<#").append(arg).append("> ");
+                    } else if (argumentSlow <= maximumSlow) {
+                        DataSource.update("UPDATE CHANNEL_SETTINGS SET MIN_SLOW = ? WHERE CHANNEL_ID = ?",
+                                Arrays.asList(Integer.toString(argumentSlow), arg));
+                        minComplete.append("<#").append(arg).append("> ");
+                    }
+
+                } catch (SQLException ex) {
+                    nonValid.append("\"").append(arg).append("\" ");
+                }
+            }
+
+        } else {
+            Messages.sendMessage(eventChannel, HelpEmbeds.helpSetBounds(eventPrefix));
+        }
+
+        // #7 - Send the results embed
         embed.setColor(0xffff00);
         if (!(complete.length() == 0)) {
-            embed.addField("Channels given a maximum slowmode of " + argumentSlow + ":", complete, false);
+            embed.addField("Channels given a maximum slowmode of " + argumentSlow + ":", complete.toString(), false);
             embed.setColor(0x00ff00);
         }
 
         if (!(badSlowmode.length() == 0)) {
-            embed.addField("Channels for which the given slowmode value was not appropriate:", badSlowmode, false);
+            embed.addField("Channels for which the given slowmode value was not appropriate:", badSlowmode.toString(), false);
         }
 
         if (!(nonValid.length() == 0)) {
-            embed.addField("Channels that were not valid or found:", nonValid, false);
+            embed.addField("Channels that were not valid or found:", nonValid.toString(), false);
         }
 
         if (!(noText.length() == 0)) {
-            embed.addField("Categories with no Text Channels:", noText, false);
+            embed.addField("Categories with no Text Channels:", noText.toString(), false);
         }
 
         embed.setTimestamp(Instant.now());
