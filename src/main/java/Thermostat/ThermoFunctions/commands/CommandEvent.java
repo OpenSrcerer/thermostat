8b98a15da66337c9ee4a52b77baadb5526052ee6
OpenSrcerer <weedpot@fhf.edu.al>
2020-10-08 18:52:38 +0300

    Fixed buggy searching of argument parsing. (finally)

--- src/main/java/Thermostat/ThermoFunctions/commands/CommandEvent.java
+++ src/main/java/Thermostat/ThermoFunctions/commands/CommandEvent.java
@@ -45,56 +45,54 @@ public interface CommandEvent {
                 nonValid = new StringBuilder(),
                 // Channels that are valid, but are not text channels
                 noText = new StringBuilder();
-
-        // parses arguments into usable IDs, checks if channels exist
-        // up to args.size(), last channel
-        for (int index = 0; index < args.size(); ++index) {
-
-            // The argument gets parsed. If it's a mention, it gets formatted
-            // into an ID through the parseMention() function.
-            // All letters are removed, thus the usage of the
-            // originalArgument string.
-            String originalArgument = args.get(index);
-            args.set(index, parseMention(args.get(index), "#"));
-
-            // Category holder for null checking
-            Category channelContainer = eventChannel.getGuild().getCategoryById(args.get(index));
-
-            if (args.get(index).isBlank()) {
-                nonValid.append("\"").append(originalArgument).append("\" ");
-                args.remove(index);
-                --index;
-
-            } else if (channelContainer != null) {
-                // firstly creates an immutable list of the channels in the category
-                List<TextChannel> TextChannels = channelContainer.getTextChannels();
-                // if list is empty add that it is in msg
-                if (TextChannels.isEmpty()) {
-                    noText.append("<#").append(originalArgument).append("> ");
-                }
-                // removes category ID from argument ArrayList
-                args.remove(index);
-                // iterates through every channel and adds its' id to the arg list
-                for (TextChannel it : TextChannels) {
-                    args.add(0, it.getId());
-                }
-                --index;
-            }
-
-            // removes element from arguments if it's not a valid channel ID
-            else if (eventChannel.getGuild().getTextChannelById(args.get(index)) == null) {
-                nonValid.append("\"").append(originalArgument).append("\" ");
-                args.remove(index);
-                --index;
-            }
-        }
+        ArrayList<String> newArgs = new ArrayList<>();
 
         // if no arguments were valid just add the event channel
         // as the target channel
         if (args.isEmpty()) {
-            args.add(eventChannel.getId());
+            newArgs.add(eventChannel.getId());
+        } else {
+            // parses arguments into usable IDs, checks if channels exist
+            // up to args.size(), last channel
+            for (int index = 0; index < args.size(); ++index) {
+
+                // The argument gets parsed. If it's a mention, it gets formatted
+                // into an ID through the parseMention() function.
+                // All letters are removed, thus the usage of the
+                // originalArgument string.
+                String originalArgument = args.get(index);
+                args.set(index, parseMention(args.get(index), "#"));
+
+                // Category holder for null checking
+                Category channelContainer = eventChannel.getGuild().getCategoryById(args.get(index));
+
+                if (args.get(index).isBlank()) {
+                    nonValid.append("\"").append(originalArgument).append("\" ");
+
+                } else if (channelContainer != null) {
+                    // firstly creates an immutable list of the channels in the category
+                    List<TextChannel> TextChannels = channelContainer.getTextChannels();
+                    // if list is empty add that it is in msg
+                    if (TextChannels.isEmpty()) {
+                        noText.append("<#").append(originalArgument).append("> ");
+                    }
+                    // iterates through every channel and adds its' id to the arg list
+                    for (TextChannel it : TextChannels) {
+                        newArgs.add(0, it.getId());
+                    }
+                }
+
+                // removes element from arguments if it's not a valid channel ID
+                else if (eventChannel.getGuild().getTextChannelById(args.get(index)) == null) {
+                    nonValid.append("\"").append(originalArgument).append("\" ");
+                }
+
+                else {
+                    newArgs.add(args.get(index));
+                }
+            }
         }
 
-        return Arrays.asList(nonValid, noText, args);
+        return Arrays.asList(nonValid, noText, newArgs);
     }
 }
