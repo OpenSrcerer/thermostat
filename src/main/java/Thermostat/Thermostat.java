bc810f974ecbe7633330f71393f41d277829051b
Weed-Pot <weedpot@fhf.edu.al>
2021-01-03 16:59:53 +0100

    Updated Thermostat version to beta3.

--- src/main/java/Thermostat/Thermostat.java
+++ src/main/java/Thermostat/Thermostat.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ScheduledExecutorService;
  * running the bot.
  *
  * @author OpenSrcerer
- * @version 1.0.0_beta2
+ * @version 1.0.0_beta3
  * @since 2020-04-17
  */
 public abstract class Thermostat {
