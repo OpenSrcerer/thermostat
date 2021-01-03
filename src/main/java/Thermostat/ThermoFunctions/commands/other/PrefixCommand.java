102bb58b13601cdf5552c909bf5579d41f85d8e6
Weed-Pot <weedpot@fhf.edu.al>
2021-01-03 17:00:35 +0100

    Improved utilization of ResponseDispatcher in PrefixCommand.

--- src/main/java/Thermostat/ThermoFunctions/commands/other/PrefixCommand.java
+++ src/main/java/Thermostat/ThermoFunctions/commands/other/PrefixCommand.java
@@ -3,13 +3,14 @@ package thermostat.thermoFunctions.commands.other;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import thermostat.Thermostat;
 import thermostat.dispatchers.ResponseDispatcher;
 import thermostat.mySQL.DataSource;
 import thermostat.preparedStatements.ErrorEmbeds;
 import thermostat.preparedStatements.GenericEmbeds;
 import thermostat.thermoFunctions.Functions;
-import thermostat.thermoFunctions.Messages;
 import thermostat.thermoFunctions.commands.Command;
+import thermostat.thermoFunctions.commands.CommandTrigger;
 import thermostat.thermoFunctions.entities.CommandType;
 
 import javax.annotation.Nonnull;
@@ -54,8 +55,11 @@ public class PrefixCommand implements Command {
             prefixAction(data, arguments, prefix);
         } catch (SQLException ex) {
             ResponseDispatcher.commandFailed(this,
-                    ErrorEmbeds.error("Try running the command again", ex.getLocalizedMessage(), Functions.getCommandId()),
-                    ex);
+                    ErrorEmbeds.error("Try running the command again",
+                            ex.getLocalizedMessage(),
+                            Functions.getCommandId()),
+                    ex
+            );
         }
     }
 
@@ -68,21 +72,38 @@ public class PrefixCommand implements Command {
     public void prefixAction(@Nonnull GuildMessageReceivedEvent data, @Nonnull List<String> arguments, @Nonnull String prefix) throws SQLException {
         if (arguments.size() > 1 && arguments.get(0).equalsIgnoreCase("set")) {
             if (Pattern.matches("[!-~]*", arguments.get(1)) && arguments.get(1).length() <= 10 && !arguments.get(1).equalsIgnoreCase(prefix)) {
-                Messages.sendMessage(data.getChannel(), GenericEmbeds.setPrefix(data.getMember().getUser().getAsTag(), data.getMember().getUser().getAvatarUrl(), arguments.get(1)));
                 DataSource.update("UPDATE GUILDS SET GUILD_PREFIX = '?' WHERE GUILD_ID = ?",
                         Arrays.asList(arguments.get(1), data.getGuild().getId()));
+                CommandTrigger.updateEntry(data.getGuild().getId(), arguments.get(1));
+                ResponseDispatcher.commandSucceeded(
+                        this, GenericEmbeds.setPrefix(
+                                data.getMember().getUser().getAsTag(),
+                                data.getMember().getUser().getAvatarUrl(),
+                                arguments.get(1)
+                        )
+                );
             } else if (arguments.get(1).equalsIgnoreCase(prefix)) {
-                Messages.sendMessage(data.getChannel(), GenericEmbeds.samePrefix(prefix));
+                ResponseDispatcher.commandFailed(this,
+                        GenericEmbeds.samePrefix(prefix),
+                        "User inserted the same prefix."
+                );
             } else {
-                Messages.sendMessage(data.getChannel(), ErrorEmbeds.incorrectPrefix(this.getId()));
+                ResponseDispatcher.commandFailed(this,
+                        ErrorEmbeds.incorrectPrefix(this.getId()),
+                        "User inserted an incorrect prefix."
+                );
             }
         } else if (arguments.size() == 1 && arguments.get(0).equalsIgnoreCase("set")) {
-            Messages.sendMessage(data.getChannel(), ErrorEmbeds.insertPrefix(this.getId()));
+            ResponseDispatcher.commandFailed(this,
+                    ErrorEmbeds.insertPrefix(this.getId()),
+                    "User did not insert a prefix."
+            );
         } else if (arguments.size() >= 1 && arguments.get(0).equalsIgnoreCase("reset")) {
             DataSource.update("UPDATE GUILDS SET GUILD_PREFIX = NULL WHERE GUILD_ID = ?", data.getGuild().getId());
-            Messages.sendMessage(data.getChannel(), GenericEmbeds.resetPrefix());
+            CommandTrigger.updateEntry(data.getGuild().getId(), Thermostat.prefix);
+            ResponseDispatcher.commandSucceeded(this, GenericEmbeds.resetPrefix());
         } else {
-            Messages.sendMessage(data.getChannel(), GenericEmbeds.getPrefix(data.getMember().getUser().getAsTag(), data.getMember().getUser().getAvatarUrl(), prefix));
+            ResponseDispatcher.commandSucceeded(this, GenericEmbeds.getPrefix(data.getMember().getUser().getAsTag(), data.getMember().getUser().getAvatarUrl(), prefix));
         }
     }
 
