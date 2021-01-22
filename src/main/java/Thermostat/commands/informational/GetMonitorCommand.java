eeb7f55fee5b8113a77a4ad3f017407bec90aa7b
Weed-Pot <weedpot@fhf.edu.al>
2021-01-22 22:29:09 +0100

    Optimized GetMonitorCommand. (Switches/DS.execute() update)

--- src/main/java/Thermostat/commands/informational/GetMonitorCommand.java
+++ src/main/java/Thermostat/commands/informational/GetMonitorCommand.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import thermostat.Embeds.ErrorEmbeds;
 import thermostat.dispatchers.ResponseDispatcher;
 import thermostat.mySQL.DataSource;
 import thermostat.Embeds.DynamicEmbeds;
@@ -12,6 +13,10 @@ import thermostat.commands.Command;
 import thermostat.util.enumeration.CommandType;
 
 import javax.annotation.Nonnull;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -38,21 +43,44 @@ public class GetMonitorCommand implements Command {
 
     /**
      * Command form: th!getmonitor
+     * No switches, no arguments.
      */
     @Override
     public void run() {
         String monitoredString = "None.", filteredString = "None.";
+        List<String> monitoredList = new ArrayList<>();
+        List<String> filteredList = new ArrayList<>();
 
         // #1 - Grab monitored and filtered channels from the DB
-        List<String> monitoredList = DataSource.queryStringArray("SELECT CHANNELS.CHANNEL_ID FROM CHANNELS " +
+        try {
+            DataSource.execute(conn -> {
+                PreparedStatement statement = conn.prepareStatement("SELECT CHANNELS.CHANNEL_ID FROM CHANNELS " +
                         "JOIN CHANNEL_SETTINGS ON (CHANNELS.CHANNEL_ID = CHANNEL_SETTINGS.CHANNEL_ID) " +
-                        "WHERE CHANNELS.GUILD_ID = ? AND CHANNEL_SETTINGS.MONITORED = 1",
-                data.getGuild().getId());
+                        "WHERE CHANNELS.GUILD_ID = ? AND CHANNEL_SETTINGS.MONITORED = 1");
+                statement.setString(1, data.getGuild().getId());
+                ResultSet rs = statement.executeQuery();
 
-        List<String> filteredList = DataSource.queryStringArray("SELECT CHANNELS.CHANNEL_ID FROM CHANNELS " +
+                while (rs.next()) {
+                    monitoredList.add(rs.getString(1));
+                }
+
+                statement = conn.prepareStatement("SELECT CHANNELS.CHANNEL_ID FROM CHANNELS " +
                         "JOIN CHANNEL_SETTINGS ON (CHANNELS.CHANNEL_ID = CHANNEL_SETTINGS.CHANNEL_ID) " +
-                        "WHERE CHANNELS.GUILD_ID = ? AND CHANNEL_SETTINGS.FILTERED = 1",
-                data.getGuild().getId());
+                        "WHERE CHANNELS.GUILD_ID = ? AND CHANNEL_SETTINGS.FILTERED = 1");
+                statement.setString(1, data.getGuild().getId());
+                rs = statement.executeQuery();
+
+                while (rs.next()) {
+                    filteredList.add(rs.getString(1));
+                }
+
+                return null;
+            });
+        } catch (SQLException ex) {
+            ResponseDispatcher.commandFailed(this,
+                    ErrorEmbeds.error(ex.getLocalizedMessage(), this.commandId), ex
+                    );
+        }
 
         // #2 - Converts channel ids to mentions
         {
@@ -79,8 +107,12 @@ public class GetMonitorCommand implements Command {
         );
     }
 
+    /**
+     * Build a String that contains channelIds, to be used in an embed.
+     * @param list List of channel IDs.
+     * @return String that contains channelIds, to be used in an embed.
+     */
     private String getEmbedString(List<String> list) {
-
         StringBuilder string = new StringBuilder();
 
         // iterate through retrieved array, adding
