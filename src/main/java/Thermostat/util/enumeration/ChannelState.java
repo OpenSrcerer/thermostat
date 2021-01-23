16ff80561edce395940060fd4fb36dc7608c0926
Weed-Pot <weedpot@fhf.edu.al>
2021-01-23 23:24:19 +0100

    Added ChannelState and DBActionType enums

---
+++ src/main/java/Thermostat/util/enumeration/ChannelState.java
@@ -0,0 +1,9 @@
+package thermostat.util.enumeration;
+
+/**
+ * An enum used for convenience in identifying channels that are
+ * monitored / filtered.
+ */
+public enum ChannelState {
+    UNMONITORED, UNFILTERED, MONITORED, FILTERED
+}
---
+++ src/main/java/Thermostat/util/enumeration/DBActionType.java
@@ -0,0 +1,21 @@
+package thermostat.util.enumeration;
+
+import javax.annotation.Nonnull;
+
+/**
+ * An enum to better organize how database actions are handled.
+ */
+public enum DBActionType {
+    FILTER("FILTERED = ?"),
+    MONITOR("MONITORED = ?"),
+    UNFILTER("FILTERED = ?, WEBHOOK_ID = 0, WEBHOOK_TOKEN = 0");
+
+    /**
+     * A String that contains the Action that the enum represents.
+     */
+    public String sqlAction;
+
+    DBActionType(final @Nonnull String sqlAction) {
+        this.sqlAction = sqlAction;
+    }
+}
